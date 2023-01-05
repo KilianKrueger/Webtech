1e4dcca8d331f0d93cf33fc61dfee74bceb054f5
Kilian Krüger <kruegerkilian@gmx.de>
2023-01-05 16:20:03 +0100

    Project

---
+++ tood/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemsComponent } from './todo-list-items/todo-list-items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoListItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ tood/src/app/todo-list-items/todo-list-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-items',
+  templateUrl: './todo-list-items.component.html',
+  styleUrls: ['./todo-list-items.component.css']
+})
+export class TodoListItemsComponent {
+
+}
